8429171a89836721d7b9e1a176ed277d0d3bc503
NILSAMAR <ing.gonzales96@gmail.com>
2024-06-06 16:23:16 -0500

    js

--- 41.JavaScriptII/41.2ImplementandoAsignaciónANDlógico&&/scripts/profile.js
+++ 41.JavaScriptII/41.2ImplementandoAsignaciónANDlógico&&/scripts/profile.js
@@ -1,25 +1,37 @@
-/*-----------------------------------------------------------------------
-41.2. ✏️ Actividad: Implementando la Asignación con AND lógico (&&)
-/*-----------------------------------------------------------------------*/
+//*-----------------------------------------------------------------------
+//*41.2. ✏️ Actividad: Implementando la Asignación con AND lógico (&&)
+//*-----------------------------------------------------------------------*/
 /*PASOS:
-💧1.Abre el archivo profile.js en Visual Studio Code.
-💧2.Define una variable isOnline y asigna un valor inicial de false. Esto representará si el usuario está conectado o no inicialmente.
-💧3.Utiliza la función prompt() para preguntar al usuario si está conectado. La pregunta debe ser clara y especificar que las respuestas posibles 
-son "SI" o "NO".
-💧4.Implementa una condición que verifique la respuesta del usuario. Convierte la respuesta a mayúsculas para evitar problemas de mayúsculas y 
-minúsculas. Si la respuesta es "SI" y (&&) isOnline es false, cambia el valor de isOnline a true.
-💧5.Dependiendo del estado de isOnline, muestra un mensaje diferente en la consola. Si isOnline es true, muestra un mensaje indicando que el 
-usuario está conectado y se mostrará el "template". Si es false, indica que el usuario no está conectado y el "template" será ocultado.
-Completa los pasos anteriores y ejecuta tu script en el navegador. Observa cómo cambian los mensajes en la consola según las diferentes respuestas 
-que ingreses en el prompt.
+//?💧1.Abre el archivo profile.js en Visual Studio Code.
+//?💧2.Define una variable isOnline y asigna un valor inicial de false. 
+//?📌Esto representará si el usuario está conectado o no inicialmente.
+//?💧3.Utiliza la función prompt() para preguntar al usuario si está conectado. 
+//?📌La pregunta debe ser clara y especificar que las respuestas posibles son "SI" o "NO".
+//?💧4.Implementa una condición que verifique la respuesta del usuario. 
+//?📌Convierte la respuesta a mayúsculas para evitar problemas de mayúsculas y minúsculas. 
+//?📌Si la respuesta es "SI" y (&&) isOnline es false, cambia el valor de isOnline a true.
+//?💧5.Dependiendo del estado de isOnline, muestra un mensaje diferente en la consola. 
+//?📌Si isOnline es true, muestra un mensaje indicando que el usuario está conectado y se mostrará el "template". 
+//?📌Si es false, indica que el usuario no está conectado y el "template" será ocultado.
+//?📌Completa los pasos anteriores y ejecuta tu script en el navegador. 
+//?📌Observa cómo cambian los mensajes en la consola según las diferentes respuestas que ingreses en el prompt.
 */
+
+
+//?💧2.Define una variable isOnline y asigna un valor inicial de false.
+//?💧3.Utiliza la función prompt() para preguntar al usuario si está conectado. 
+
 let isOnline = false; //Varible isOnline, valor inicial False
 const respuesta = prompt("¿Estás conectado? Responde 'SI' o 'NO'");//Funcion prompt
 const respuestaMayuscula = respuesta.toUpperCase();//se comvierte la respuesta en mayuscula
 
+//?💧4.Implementa una condición que verifique la respuesta del usuario. 
+
 if (respuestaMayuscula === "SI" && !isOnline) {//condicional , si la respuesta es :SI & isOnline=false
     isOnline = true;
 }
+
+//?💧5.Dependiendo del estado de isOnline, muestra un mensaje diferente en la consola. 
 
 if (isOnline) {
     console.log("El usuario está conectado. Mostrando el perfil...");
--- 43.Funciones&Modulos/43.3ModularizacionDatos/geometric.js
+++ 43.Funciones&Modulos/43.3ModularizacionDatos/geometric.js
@@ -7,28 +7,28 @@ const tableBody = document.getElementById('table-body');
 
 rectangulos.forEach((rect, index) => {
     const row = document.createElement('tr');
-    
+
     const rectName = document.createElement('td');
     rectName.textContent = `Rectángulo ${index + 1}`;
-    
+
     const rectAncho = document.createElement('td');
     rectAncho.textContent = rect.ancho;
-    
+
     const rectAltura = document.createElement('td');
     rectAltura.textContent = rect.altura;
-    
+
     const rectPerimetro = document.createElement('td');
     rectPerimetro.textContent = perimetro(rect);
-    
+
     const rectArea = document.createElement('td');
     rectArea.textContent = area(rect);
-    
+
     row.appendChild(rectName);
     row.appendChild(rectAncho);
     row.appendChild(rectAltura);
     row.appendChild(rectPerimetro);
     row.appendChild(rectArea);
-    
+
     tableBody.appendChild(row);
 });
 
@@ -43,3 +43,34 @@ geometric/
 └── rectangleData.js
 
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
